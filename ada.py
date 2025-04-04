a99c468d59111dfeac98d6f6a6cc753c7cfaf9c4
bautigq <bgonzalezquesnel1@mail.austral.edu.ar>
2025-04-04 10:48:45 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    
+print(first_name.lower(), last_name.lower()) 
+print(first_name.capitalize(), last_name.capitalize()) 
+print(first_name.upper(), last_name.upper()) 
+print("\t" + first_name.lower(), last_name.lower())
