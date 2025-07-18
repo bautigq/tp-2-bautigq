ec2a2b259c7c9f7d92d96cca503dc67b7376a15f
bautigq <bgonzalezquesnel1@mail.austral.edu.ar>
2025-07-18 13:10:12 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,7 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-result1 = x < y result2 = y < x
-
-print(f"The result of {x} comes first in the dictionary than {y} is {result1}.") 
-print(f"The result of {y} comes first in the dictionary than {x} is {result2}.")
+    a = x < y 
+    b = y < x
+    print(f"The result of {x} comes first in the dictionary than {y} is {a}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {b}.")
